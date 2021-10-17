c0f28c04944ba6ff77056a6ed81b3334e551a806
HelderVerifaction <helder@verifaction.co.za>
2021-10-17 19:33:27 +0200

    Changed to nested routing

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,9 +1,48 @@
 class MembersController < ApplicationController
+    before_action :set_product, only: %i[show update destroy]
+    
     def index
         render json: Member.all
     end
 
     def show
-        render json: Member.find(params[:id])
+        render json: @member
     end
+
+    def create
+        current_club = Club.find(params[:club_id])
+        member = current_club.members.build(member_params)
+        if member.save
+            render json: member, status: :created
+        else
+            render json: { errors: member.errors }, status:
+            :unprocessable_entity
+        end
+    end
+
+    def update
+        if @member.update(member_params)
+            render json: @member
+        else
+            render json: @member.errors, status:
+            :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @member.destroy
+        head 204
+    end
+    
+    private
+
+    def member_params
+        params.require(:member).permit(:first_name, :last_name, :email)
+        end
+
+    def set_member
+        @member = Member.find(params[:id])
+    end
+    
+    
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,8 @@
 class Member < ApplicationRecord
     belongs_to :club
+    after_initialize :init
+
+    def init
+      self.games_played = 0          
+    end
 end
